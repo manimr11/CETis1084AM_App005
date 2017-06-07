3b0ca4301548031499aed361e2c4b55d53d2c471
Machado Avitia Manuel Enrique <15325061080136@cetis108.edu.mx>
2017-06-07 13:55:05 -0600

    v1

---
+++ app/src/main/java/mx/edu/cetis108/cetis1084am_app005/Archivo.java
@@ -0,0 +1,17 @@
+package mx.edu.cetis108.cetis1084am_app005;
+
+import java.util.Date;
+
+/**
+ * Created by manue on 07/06/2017.
+ */
+
+public class Archivo {
+    String usuario;
+    String nombre;
+    String tipo;
+    Date fecha_Creacion;
+    Date fecha_Modificacion;
+    String ruta;
+
+}
